71fa0835380796a15dec0ebdd7f3782891c55a1d
Matt Peters <mape0744@colorado.edu>
2022-05-03 12:30:00 -0600

    g

--- Java/tic.js
+++ Java/tic.js
@@ -1,89 +1,97 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
 
-startGame()
+var cell = new Array(9);
+for (var i=0; i<9; i++) cell[i] = 0;
+var items=document.querySelectorAll(".item");
 
-restartButton.addEventListener('click', startGame)
 
-function startGame() {
-  circleTurn = false
-  cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(CIRCLE_CLASS)
-    cell.removeEventListener('click', handleClick)
-    cell.addEventListener('click', handleClick, { once: true })
-  })
-  setBoardHoverClass()
-  winningMessageElement.classList.remove('show')
+function Cross(value) {
+  items[value].innerHTML="X";
+
 }
 
-function handleClick(e) {
-  const cell = e.target
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-  placeMark(cell, currentClass)
-  if (checkWin(currentClass)) {
-    endGame(false)
-  } else if (isDraw()) {
-    endGame(true)
-  } else {
-    swapTurns()
-    setBoardHoverClass()
+function Zero(value) {
+  items[value].innerHTML="O";
+
+}
+
+function CheckVictory() {
+  if (cell[0] == cell[1] && cell[1] == cell[2] && cell[2] > 0) return true;
+  if (cell[3] == cell[4] && cell[4] == cell[5] && cell[5] > 0) return true;
+  if (cell[6] == cell[7] && cell[7] == cell[8] && cell[8] > 0) return true;
+  if (cell[6] == cell[3] && cell[3] == cell[0] && cell[0] > 0) return true;
+  if (cell[7] == cell[4] && cell[4] == cell[1] && cell[1] > 0) return true;
+  if (cell[8] == cell[5] && cell[5] == cell[2] && cell[2] > 0) return true;
+  if (cell[6] == cell[4] && cell[4] == cell[2] && cell[2] > 0) return true;
+  if (cell[0] == cell[4] && cell[4] == cell[8] && cell[8] > 0) return true;
+}
+
+function CompTurn() {
+  for (i=0; i<9; i++) if (cell[i] == 0) PutHere = i;
+  for (i=0; i<3; i++) {
+  if (cell[0] == cell[1] && cell[2] == 0 && cell[0] == i) PutHere = 2;
+  if (cell[0] == cell[2] && cell[1] == 0 && cell[0] == i) PutHere = 1;
+  if (cell[1] == cell[2] && cell[0] == 0 && cell[2] == i) PutHere = 0;
+  if (cell[3] == cell[4] && cell[5] == 0 && cell[3] == i) PutHere = 5;
+  if (cell[3] == cell[5] && cell[4] == 0 && cell[3] == i) PutHere = 4;
+  if (cell[4] == cell[5] && cell[3] == 0 && cell[5] == i) PutHere = 3;
+  if (cell[6] == cell[7] && cell[8] == 0 && cell[6] == i) PutHere = 8;
+  if (cell[6] == cell[8] && cell[7] == 0 && cell[6] == i) PutHere = 7;
+  if (cell[7] == cell[8] && cell[6] == 0 && cell[8] == i) PutHere = 6;
+
+  if (cell[6] == cell[3] && cell[0] == 0 && cell[6] == i) PutHere = 0;
+  if (cell[6] == cell[0] && cell[3] == 0 && cell[6] == i) PutHere = 3;
+  if (cell[3] == cell[0] && cell[6] == 0 && cell[3] == i) PutHere = 6;
+  if (cell[7] == cell[4] && cell[1] == 0 && cell[7] == i) PutHere = 1;
+  if (cell[7] == cell[1] && cell[4] == 0 && cell[7] == i) PutHere = 4;
+  if (cell[4] == cell[1] && cell[7] == 0 && cell[4] == i) PutHere = 7;
+  if (cell[8] == cell[5] && cell[2] == 0 && cell[8] == i) PutHere = 2;
+  if (cell[8] == cell[2] && cell[5] == 0 && cell[8] == i) PutHere = 5;
+  if (cell[5] == cell[2] && cell[8] == 0 && cell[5] == i) PutHere = 8;
+
+  if (cell[6] == cell[4] && cell[2] == 0 && cell[6] == i) PutHere = 2;
+  if (cell[6] == cell[2] && cell[4] == 0 && cell[6] == i) PutHere = 4;
+  if (cell[4] == cell[2] && cell[6] == 0 && cell[4] == i) PutHere = 6;
+  if (cell[0] == cell[4] && cell[8] == 0 && cell[0] == i) PutHere = 8;
+  if (cell[0] == cell[8] && cell[4] == 0 && cell[0] == i) PutHere = 4;
+  if (cell[4] == cell[8] && cell[0] == 0 && cell[4] == i) PutHere = 0;
+  }
+  Zero(PutHere);
+  cell[PutHere] = 2;
+  if (CheckVictory() == true) {
+    alert("JavaScript the BEST!!!");
+    GameOver();
   }
 }
 
-function endGame(draw) {
-  if (draw) {
-    winningMessageTextElement.innerText = 'Draw!'
-  } else {
-    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-  }
-  winningMessageElement.classList.add('show')
-}
-
-function isDraw() {
-  return [...cellElements].every(cell => {
-    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-  })
-}
-
-function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass)
-}
-
-function swapTurns() {
-  circleTurn = !circleTurn
-}
-
-function setBoardHoverClass() {
-  board.classList.remove(X_CLASS)
-  board.classList.remove(CIRCLE_CLASS)
-  if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS)
-  } else {
-    board.classList.add(X_CLASS)
+function GameOver() {
+  for (i=0; i<9; i++)
+  {
+    cell[i] = 0;
+    items[i].innerHTML='';
   }
 }
 
-function checkWin(currentClass) {
-  return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
-      return cellElements[index].classList.contains(currentClass)
-    })
-  })
+function CheckNobody() {
+  no = false;
+  for (i=0; i<9; i++) if (cell[i] == 0) no = true;
+  if (no == false) {
+    alert("Draw");
+    GameOver();
+  }
+}
+
+function Place(value) {
+  if (cell[value] == 0) {
+    Cross(value);
+    cell[value] = 1;
+    if (CheckVictory() == true) {
+      alert("Congrats!!! You won");
+      GameOver();
+    }
+    else {
+      CheckNobody();
+      CompTurn();
+      CheckNobody();
+    }
+  }
 }
